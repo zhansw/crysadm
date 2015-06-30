a3ce5ae7f005b0d2617f9b01a8294dce61c285e8
powergx <powergx@gmail.com>
2015-06-30 09:30:05 +0800

    new project

--- XunleiCrystal.py
+++ XunleiCrystal.py
@@ -22,10 +22,10 @@ def hello_world():
     cookies = dict(sessionid='C3F6EA0EA3E1A7908109D012F71D458B',userid='266244981')
     headers = {'User-Agent': 'RedCrystal/1.4.0 (iPhone; iOS 8.3; Scale/2.00)'}
     s = requests.Session()
-    r = s.get('https://red.xunlei.com/?r=mine/devices_stat', cookies=cookies,headers=headers)
+    r = s.get('https://red.xunlei.com/?r=mine/info', cookies=cookies,headers=headers)
     print(r.text)
 
-    print(r.get('https://red.xunlei.com/?r=mine/speed_stat', cookies=cookies, headers=headers).text)
+    print(s.get('https://red.xunlei.com/?r=mine/devices_stat', cookies=cookies,headers=headers).text)
     return r.text
 
 
