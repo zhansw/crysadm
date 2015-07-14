eb83a6980d48913abe454dbb050e0e13525547c2
powergx <powergx@gmail.com>
2015-07-14 11:55:42 +0800

    登陆成功

--- login.py
+++ login.py
@@ -1,25 +1,13 @@
 __author__ = 'powergx'
 import requests
-import time, re
 import random
 import json
-
-
-def current_timestamp():
-    return int(time.time() * 1000)
 
 
 def md5(s):
     import hashlib
 
     return hashlib.md5(s.encode('utf-8')).hexdigest().lower()
-
-
-def encypt_password(password):
-    if not re.match(r'^[0-9a-f]{32}$', password):
-        password = md5(md5(password))
-    return password
-
 
 def StrToInt(str):
     bigInteger = 0
@@ -43,7 +31,6 @@ def pow_mod(x, y, z):
 
 
 def login(username, password):
-    session = requests.session()
     exponent = int("010001", 16)
     modulus = int("D6F1CFBF4D9F70710527E1B1911635460B1FF9AB7C202294D04A6F135A906E90E2398123C234340A3CEA0E5EFDC"
                   "B4BCF7C613A5A52B96F59871D8AB9D240ABD4481CCFD758EC3F2FDD54A1D4D56BFFD5C4A95810A8CA25E87FDC75"
@@ -56,20 +43,16 @@ def login(username, password):
             '"verifyKey":"","sessionID":"","protocolVersion":100,"userName":"%s","extensionList":"",' \
             '"sequenceNo":10000001,"peerID":"%s","clientVersion":"1.0.0"}'
 
-    hash_password = hex(pow_mod(StrToInt(md5(password)), exponent, modulus))[2:]
+    hash_password = hex(pow_mod(StrToInt(md5(password)), exponent, modulus))[2:].upper()
 
-    _chars = "0123456789ABCDEF";
+    _chars = "0123456789ABCDEF"
 
     peer_id = ''.join(random.sample(_chars, 16))
 
     param = param % (hash_password, username, peer_id)
-    params = json.loads(param)
-    headers = {'user-agent': "android-async-http/1.4.3 (http://loopj.com/android-async-http)"}
-    text2 = session.get("https://login.mobile.reg2t.sandai.net/", params=params, headers=headers);
 
+    headers = {'user-agent': "RedCrystal/1.5.0 (iPhone; iOS 8.4; Scale/2.00)"}
+    r = requests.post("https://login.mobile.reg2t.sandai.net/", data=param, headers=headers, verify=False)
 
-    print(param)
-
-
-if __name__ == '__main__':
-    login('powergx@gmail.com', '123456')
+    login_status = json.loads(r.text)
+    return login_status
