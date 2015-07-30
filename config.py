8fd048b8e56b1a2e78ecf5f716d79c25be1d9241
powergx <powergx@gmail.com>
2015-07-30 22:57:55 +0800

    Merge commit '20fc3c9f02be2d83a8252f6e1fdc0d43b3539bda' into dev
    * commit '20fc3c9f02be2d83a8252f6e1fdc0d43b3539bda':
      ssl 日志报错
      support customized server port and ip in config.py
      update config.py  SERVER_PORT to int
    
    Conflicts:
    	crysadm.py

--- config.py
+++ config.py
@@ -15,18 +15,19 @@ class Config(object):
     ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
     SESSION_TYPE = 'memcached'
     SECRET_KEY = '7e30485a-dd01-11e4-8abd-10ddb199c373'
-    REDIS_CONF = RedisConfig(host='127.0.0.1', port=6379, db=0)
-    PASSWORD_PREFIX = "08b3db21-d120-11e1-9ttd-10ddb199c373"
-    ENCRYPT_PWD_URL = ''
-    SERVER_IP = '0.0.0.0'
+    REDIS_CONF = RedisConfig(host='10.0.0.29', port=6379, db=0)
+    PASSWORD_PREFIX = "08b3db21-d120-11e4-9ttd-10ddb199c373"
+    ENCRYPT_PWD_URL = 'http://10.0.0.29:9898/'
+    SERVER_IP = '127.0.0.1'
     SERVER_PORT = 5000
 
 
 class ProductionConfig(Config):
-    pass
+    DEBUG = True
 
 
 class DevelopmentConfig(Config):
+    REDIS_CONF = RedisConfig(host='airl.us', port=46378, db=0)
     DEBUG = True
 
 
